9019ce820a7f2e4d09e327eaad60c92fec334f90
Norbert Funke <norbert@funke.com>
2021-06-14 11:19:42 -0500

    Initial migration

--- pre-processing/pre-processing-code/lambda_function.py
+++ pre-processing/pre-processing-code/lambda_function.py
@@ -3,149 +3,64 @@ import json
 from source_data import source_dataset
 import boto3
 import os
-from datetime import date
+from datetime import date, datetime
+from multiprocessing.dummy import Pool
+
+s3_resource = boto3.resource('s3')
 
 os.environ['AWS_DATA_PATH'] = '/opt/'
 
 dataexchange = boto3.client(
-    service_name='dataexchange',
-    region_name=os.environ['REGION']
+	service_name='dataexchange',
+	region_name=os.environ['REGION']
 )
 
 marketplace = boto3.client(
-    service_name='marketplace-catalog',
-    region_name=os.environ['REGION']
+	service_name='marketplace-catalog',
+	region_name=os.environ['REGION']
 )
 
-s3_bucket = os.environ['S3_BUCKET']
-data_set_arn = os.environ['DATA_SET_ARN']
-data_set_id = data_set_arn.split("/", 1)[1]
-product_id = os.environ['PRODUCT_ID']
-data_set_name = os.environ['DATA_SET_NAME']
-new_s3_key = data_set_name + '/dataset/'
-cfn_template = data_set_name + '/automation/cloudformation.yaml'
-post_processing_code = data_set_name + '/automation/post-processing-code.zip'
+product_id = os.getenv("PRODUCT_ID", "blank")
+dataset_arn = os.getenv("DATASET_ARN")
+dataset_id = dataset_arn.split('/', 1)[1]
+dataset_name = os.getenv("DATASET_NAME")
 
-today = date.today()
-revision_comment = 'Revision Updates v' + today.strftime('%Y-%m-%d')
+asset_bucket = os.getenv("ASSET_BUCKET")
+manifest_bucket = os.getenv("MANIFEST_BUCKET")
+customer_id = os.getenv("CUSTOMER_ID")
 
-if not s3_bucket:
-    raise Exception("'S3_BUCKET' environment variable must be defined!")
+timestamp = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+revision_comment = 'Revision Updates v' + timestamp
 
-if not new_s3_key:
-    raise Exception("'DATA_SET_NAME' environment variable must be defined!")
-
-if not data_set_arn:
-    raise Exception("'DATA_SET_ARN' environment variable must be defined!")
-
-if not product_id:
-    raise Exception("'PRODUCT_ID' environment variable must be defined!")
+required_env_vars = [ product_id, dataset_arn, dataset_name, asset_bucket, manifest_bucket, customer_id ]
+for v in required_env_vars:
+	if not v:
+		raise Exception(f"'{v.upper()}' environment variable must be defined!")
 
 
-def start_change_set(describe_entity_response, revision_arn):
-    # Call AWSMarketplace Catalog's start-change-set API to add revisions to the Product
-
-    change_details = {
-        'DataSetArn': data_set_arn,
-        'RevisionArns': [
-            revision_arn
-        ]
+def upload_manifest_file(asset_list):
+    """
+        Generates and uploads a manifest file to the MANIFEST_BUCKET of the
+        Rearc Data Platform or AWS Data Exchange Publisher Coordinator
+        https://github.com/rearc-data/aws-data-exchange-publisher-coordinator
+    """
+    manifest_file_data = {
+        "product_id": product_id,
+        "dataset_id": dataset_id,
+        "asset_list": asset_list
     }
 
-    change_set = [
-        {
-            'ChangeType': 'AddRevisions',
-            'Entity': {
-                'Identifier': describe_entity_response['EntityIdentifier'],
-                'Type': describe_entity_response['EntityType']
-            },
-            'Details': json.dumps(change_details)
-        }
-    ]
+    manifest_file_name = f"manifest-{timestamp}.json"
+    manifest_object_key = os.path.join(customer_id, product_id, dataset_id, manifest_file_name)
 
-    response = marketplace.start_change_set(
-        Catalog='AWSMarketplace', ChangeSet=change_set)
-    return response
+    s3_resource.Object(manifest_bucket, manifest_object_key).put(Body=json.dumps(manifest_file_data))
 
 
 def lambda_handler(event, context):
-    asset_list = source_dataset(data_set_name, s3_bucket, new_s3_key)
+    asset_list = source_dataset()
 
-    if type(asset_list) == list:
-        if len(asset_list) == 0:
-            print(
-                'No need for a revision, all datasets included with this product are up to date')
-            return {
-                'statusCode': 200,
-                'body': json.dumps('No need for a revision, all datasets included with this product are up to date')
-            }
-
-        create_revision_response = dataexchange.create_revision(
-            DataSetId=data_set_id)
-        revision_id = create_revision_response['Id']
-        revision_arn = create_revision_response['Arn']
-
-        # Used to store the Ids of the Jobs importing the assets to S3.
-        job_ids = set()
-
-        import_job = dataexchange.create_job(
-            Type='IMPORT_ASSETS_FROM_S3',
-            Details={
-                'ImportAssetsFromS3': {
-                    'DataSetId': data_set_id,
-                    'RevisionId': revision_id,
-                    'AssetSources': asset_list
-                }
-            }
-        )
-
-        # Start the Job and save the JobId.
-        dataexchange.start_job(JobId=import_job['Id'])
-        job_ids.add(import_job['Id'])
-
-        # Iterate until all remaining jobs have reached a terminal state, or an error is found.
-        completed_jobs = set()
-
-        while job_ids != completed_jobs:
-            for job_id in job_ids:
-                if job_id in completed_jobs:
-                    continue
-                get_job_response = dataexchange.get_job(JobId=job_id)
-                if get_job_response['State'] == 'COMPLETED':
-                    print("Job {} completed".format(job_id))
-                    completed_jobs.add(job_id)
-                if get_job_response['State'] == 'ERROR':
-                    job_errors = get_job_response['Errors']
-                    raise Exception(
-                        'JobId: {} failed with errors:\n{}'.format(job_id, job_errors))
-                # Sleep to ensure we don't get throttled by the GetJob API.
-                time.sleep(0.2)
-
-        update_revision_response = dataexchange.update_revision(
-            DataSetId=data_set_id, RevisionId=revision_id, Comment=revision_comment, Finalized=True)
-
-        revision_state = update_revision_response['Finalized']
-
-        if revision_state == True:
-            # Call AWSMarketplace Catalog's APIs to add revisions
-            describe_entity_response = marketplace.describe_entity(
-                Catalog='AWSMarketplace', EntityId=product_id)
-            start_change_set_response = start_change_set(
-                describe_entity_response, revision_arn)
-            if start_change_set_response['ChangeSetId']:
-                return {
-                    'statusCode': 200,
-                    'body': json.dumps('Revision updated successfully and added to the dataset')
-                }
-            else:
-                return {
-                    'statusCode': 500,
-                    'body': json.dumps('Something went wrong with AWSMarketplace Catalog API')
-                }
-        else:
-            return {
-                'statusCode': 500,
-                'body': json.dumps('Revision did not complete successfully')
-            }
-    else:
-        raise Exception('Something went wrong when uploading files to s3')
+    if asset_list:
+        try:
+            upload_manifest_file(asset_list)
+        except Exception as e:
+            raise Exception(f"Something went wrong when uploading manifest file to manifest bucket: {e}")
---
+++ run.sh
@@ -0,0 +1,36 @@
+export ASSET_BUCKET='rearc-data-provider'
+export MANIFEST_BUCKET='norbert-test-adx-publisher-coordinator-manifest-bucket'
+export CUSTOMER_ID='rearc'
+export DATASET_NAME='nyt-states-reopen-status-covid-19'
+export DATASET_ARN='arn:aws:dataexchange:us-east-1:796406704065:data-sets/6afda64ca351e462006618317d6c2f7b'
+export PRODUCT_NAME='COVID-19 United States Reopen and Shut Down Status by State | NY Times'
+export PRODUCT_ID='prod-csznmknbjsrn6'
+export SCHEDULE_CRON="cron(0 9 ? * 3 *)"
+export REGION='us-east-1'
+# export PROFILE='guardian-pg'
+
+echo "AssetBucket: $ASSET_BUCKET"
+echo "ManifestBucket: $MANIFEST_BUCKET"
+echo "CustomerId: $CUSTOMER_ID"
+echo "DataSetName: $DATASET_NAME"
+echo "DataSetArn: $DATASET_ARN"
+echo "ProductName: $PRODUCT_NAME"
+echo "ProductID: $PRODUCT_ID"
+echo "ScheduleCron: $SCHEDULE_CRON"
+echo "Region: $REGION"
+echo "PROFILE: $PROFILE"
+
+# python pre-processing/pre-processing-code/source_data.py
+
+./init.sh \
+    --schedule_cron "${SCHEDULE_CRON}" \
+    --asset-bucket "${ASSET_BUCKET}" \
+    --manifest-bucket "${MANIFEST_BUCKET}" \
+    --customer-id "${CUSTOMER_ID}" \
+    --dataset-name "${DATASET_NAME}" \
+    --product-name "${PRODUCT_NAME}" \
+    --product-id "${PRODUCT_ID}" \
+    --region "${REGION}" \
+    --first_revision "false"
+    # --profile "${PROFILE}"
+
